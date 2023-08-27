23e17f2340fdb74c5cfdc4f7245bb4732539d6ea
esquivelgor <esquivelguillerm@gmail.com>
2023-08-26 19:53:13 -0700

    Initial Commit

---
+++ BAMX Incidencias/BAMX_IncidenciasApp.swift
@@ -0,0 +1,17 @@
+//
+//  BAMX_IncidenciasApp.swift
+//  BAMX Incidencias
+//
+//  Created by velgor on 26/08/23.
+//
+
+import SwiftUI
+
+@main
+struct BAMX_IncidenciasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BAMX Incidencias/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BAMX Incidencias
+//
+//  Created by velgor on 26/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
