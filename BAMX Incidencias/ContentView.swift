8ccb07bd50b39ed7958bf5c841c19e748bdd88a6
esquivelgor <esquivelguillerm@gmail.com>
2023-08-27 14:52:33 -0700

    Creation of loading view

--- BAMX Incidencias/ContentView.swift
+++ BAMX Incidencias/ContentView.swift
@@ -9,11 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.blue
+                .ignoresSafeArea()
+            Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/)
+                .foregroundColor(.white)
+                .font(.system(size: 30))
+
+        }
     }
 }
-
+            
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
