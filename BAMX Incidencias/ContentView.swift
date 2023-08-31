8d92c8b332a476e753634ac87f5eb374f157c640
velgor <esquivelguillerm@gmail.com>
2023-08-30 20:23:42 -0600

    first version of login

--- BAMX Incidencias/ContentView.swift
+++ BAMX Incidencias/ContentView.swift
@@ -8,14 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isShowingDetailView = false
+    
     var body: some View {
-        ZStack {
-            Color.blue
-                .ignoresSafeArea()
-            Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/)
-                .foregroundColor(.white)
-                .font(.system(size: 30))
-
+        NavigationView{
+            VStack(spacing: 30){
+                Image("bamx")
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 250, height: 250, alignment: .center)
+                    .padding()
+                
+                Text("Iniciar Sesión")
+                    .fontWeight(.bold)
+                    .frame(width: 170, height: 40, alignment: .center)
+                    .background(Color.red)
+                    .foregroundColor(.white)
+                    .cornerRadius(100)
+                
+                NavigationLink(
+                    destination: SplashScreenView(),
+                    label: {
+                        Text("Registrarme")
+                    }
+                )
+            }
+            .navigationTitle("Login")
         }
     }
 }
